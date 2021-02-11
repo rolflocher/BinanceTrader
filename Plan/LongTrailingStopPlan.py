86f9d34f1023a11c55c8ca365b859d76f8a21496
rolflocher <rolflocher47@gmail.com>
2021-02-11 10:29:18 -0500

    Update LongTrailingStopPlan.py

--- Plan/LongTrailingStopPlan.py
+++ Plan/LongTrailingStopPlan.py
@@ -5,14 +5,12 @@ from .Position import Position
 class LongTrailingStopPlan(APlan):
 
 	def __init__(self):
-#		self.trailingStopPrice = None
 		self.trailingStopPercent = None
 
 	def trade(self, klines, positions, lead, base) -> list:
 		if len(positions) == 0:
 			if lead == True:
 				self.trailingStopPercent = self.params[0]
-#				self.trailingStopPrice = klines[-1].close * ((100 - self.trailingStopPercent) / 100)
 				buyOrder = Order(
 					"MARKET",
 					base/klines[-1].close,
@@ -30,12 +28,6 @@ class LongTrailingStopPlan(APlan):
 					self.trailingStopPercent,
 					True)
 				return [buyOrder, trailingOrder]
-#		else:
-#			if klines[-1].low < self.trailingStopPrice:
-#				return Order()
-#			if klines[-1].high * ((100 - self.trailingStopPercent) / 100) > self.trailingStopPrice:
-#				self.trailingStopPrice = klines[-1].high * ((100 - self.trailingStopPercent) / 100)
-			
 		return []
 
 	def validateParams(self, params) -> bool:
